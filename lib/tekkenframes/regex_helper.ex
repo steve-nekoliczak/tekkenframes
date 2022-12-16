d048a1916399049e49c50c272dcd973bdbc9ed57
steve-nekoliczak <steve.nekoliczak@protonmail.com>
2022-12-15 18:09:07 -0600

    change strings in regex helper to sigils

--- lib/tekkenframes/regex_helper.ex
+++ lib/tekkenframes/regex_helper.ex
@@ -3,26 +3,26 @@ defmodule Tekkenframes.RegexHelper do
   Regexes to help validate moves. Used in the move model.
   """
 
-  @stance           "(SS|SSR|SSL|WS|FC|TC|TJ)"
-  @move_input       "(u|u\/f|f|d\/f|d|d\/b|b|u\/b|U|U\/F|F|D\/F|D|D\/B|B|U\/B|qcb|qcf|hcb|hcf)"
-  @attack_input     "(1|1\\+2|1\\+2\\+3|1\\+2\\+3\\+4|1\\+3|1\\+3\\+4|1\\+4|2|2\\+3|2\\+3\\+4|2\\+4|3|3\\+4|4)"
-  @input_entry      "(#{@move_input}|#{@attack_input}|#{@move_input}\\+#{@attack_input})"
-  @input            "(#{@stance} )?#{@input_entry}(, (#{@input_entry}|N))*"
+  @stance           ~S((SS|SSR|SSL|WS|FC|TC|TJ\))
+  @move_input       ~S((u|u/f|f|d/f|d|d/b|b|u/b|U|U/F|F|D/F|D|D/B|B|U/B|qcb|qcf|hcb|hcf\))
+  @attack_input     ~S((1|1\+2|1\+2\+3|1\+2\+3\+4|1\+3|1\+3\+4|1\+4|2|2\+3|2\+3\+4|2\+4|3|3\+4|4\))
+  @input_entry      ~s((#{@move_input}|#{@attack_input}|#{@move_input}\\+#{@attack_input}\))
+  @input            ~s((#{@stance} \)?#{@input_entry}(, (#{@input_entry}|N\)\)*)
   @input_regex      ~r/^#{@input}$/
   def input_regex, do: @input_regex
 
-  @damage_regex     ~r/^\d*(, \d*)*$/
+  @damage_regex     ~R/^\d*(, \d*)*$/
   def damage_regex, do: @damage_regex
 
-  @hit_level        "(h|m|l)"
-  @hit_level_entry  "#{@hit_level}( \\(#{@stance}\\))?"
+  @hit_level        ~S((h|m|l\))
+  @hit_level_entry  ~s(#{@hit_level}( (#{@stance}\)\)?)
   @hit_level_regex  ~r/^(#{@hit_level_entry}(, #{@hit_level_entry})*\!?|\!)$/
   def hit_level_regex, do: @hit_level_regex
 
-  @frames_entry      "((\\+|\-){1}\\d+|0)"
-  @frames_regex       ~r/^(#{@frames_entry}(~#{@frames_entry})?)$/
+  @frames_entry     ~S(((\+|-\){1}\d+|0\))
+  @frames_regex     ~r/^(#{@frames_entry}(~#{@frames_entry})?)$/
   def frames_regex, do: @frames_regex
 
-  @start_up_regex     ~r/^\d+(~\d+)?$/
+  @start_up_regex   ~R/^\d+(~\d+)?$/
   def start_up_regex, do: @start_up_regex
 end
